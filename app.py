fa3dd2b3bcfc68b6b7b9d9c98ae10b00d0e53077
dStass <dstiasny@outlook.com>
2020-08-07 16:41:03 +1000

    image/layer restructuring

--- app.py
+++ app.py
@@ -15,22 +15,25 @@ from processing.inpaint_processing import ImageInpainter
 
 # import image
 IMG_PATH = 'assets/spaceman.jpg'
+NUM_LAYERS = 2
 
 ext = IMG_PATH.split('.')[1]
 io = ImageIO(ext)
 
-layers = []
+loaded_images = io.load_recursive_quantised(IMG_PATH, 20, 2, 8)
+images = [[li] for li in loaded_images]
 
-# load layers
-layers.append(io.load_recursive_quantised(IMG_PATH, 20, 2, 8))  # photos, edges, etc.
-layers.append([CannyEdgeDetection().generate_edges(im) for im in layers[0]])
-# loaded_edges = [EntropyEdgeDetection().generate_binary_edges_heatmap(im) for im in loaded_images]
+for i in range(len(images)):
+  layer = images[i]
+  layer.append(CannyEdgeDetection().generate_edges(layer[0]))
+
 
 # display settings
+image_index = 0
 layer_index = 0
-display_index = 0
-current_layer = [np.copy(l) for l in layers[layer_index]]
-current_display = current_layer[display_index]
+
+current_layers = [np.copy(l) for l in images[image_index]]
+current_display = current_layers[layer_index]
 
 # cursor settings
 show_edges_bool = False
@@ -44,25 +47,32 @@ mouse_y = -1
 
 # mouse events
 def mouse_callback(event, x, y, flags, param):
+  def draw_circle():
+    alpha_value = 1
+
+    # -1 to fill the circle
+    for i, layer in enumerate(current_layers):
+      overlay = layer.copy()
+      cv2.circle(overlay, (x,y), MOUSE_RADIUS, FILL_COLOUR, -1) 
+      # image_to_show = cv2.addWeighted(overlay, alpha_value, image_to_show, 1 - alpha_value, 0)
+      current_layers[i] = cv2.addWeighted(overlay, alpha_value, layer, 1 - alpha_value, 0)
+      current_display = current_layers[i]
+
   global current_display, mouse_pressed, mouse_x, mouse_y
 
   if event == cv2.EVENT_LBUTTONDOWN:
     mouse_pressed = True
+    draw_circle()
     # s_x, s_y = x, y
     print(x, y)
     # image_to_show = cv2.rectangle(np.copy(current_display[current_image_index]), (0, 0), (60, 60), (0, 255, 0), 2)
     
     # image_to_show = np.copy(np.copy(loaded_images[0]))
 
-  # elif event == cv2.EVENT_MOUSEMOVE:
-  #   if not mouse_pressed:
-  #     overlay = current_display[current_image_index].copy()
-  #     alpha_value = 0.1
-
-  #     # -1 to fill the circle
-  #     cv2.circle(overlay, (x,y), MOUSE_RADIUS, FILL_COLOUR, -1) 
-  #     image_to_show = cv2.addWeighted(overlay, alpha_value, image_to_show, 1 - alpha_value, 0)
-  #     # cv2.rectangle(image_to_show, (s_x, s_y), (x, y), (0, 255, 0), 1)
+  elif event == cv2.EVENT_MOUSEMOVE:
+    if mouse_pressed:
+      draw_circle()
+      # cv2.rectangle(image_to_show, (s_x, s_y), (x, y), (0, 255, 0), 1)
 
   elif event == cv2.EVENT_LBUTTONUP:
     mouse_pressed = False
@@ -104,14 +114,24 @@ while True:
 
   # cycle through images
   # cycle left
+
+  # handle layers
+  elif k == ord('l'):
+    layer_index += 1
+    layer_index %= NUM_LAYERS
+    # current_layers = [np.copy(l) for l in images[image_index]]
+    current_display = current_layers[layer_index]
+
   elif k == 123 or k == ord(','):
-    if display_index > 0: display_index -= 1
-    current_display = np.copy(np.copy(current_layer[display_index]))
+    if image_index > 0: image_index -= 1
+    current_layers = [np.copy(l) for l in images[image_index]]
+    current_display = current_layers[layer_index]
   
   # cycle right
   elif k == 124 or k == ord('.'):
-    if display_index < len(current_layer) - 1: display_index += 1
-    current_display = np.copy(np.copy(current_layer[display_index]))
+    if image_index < len(images) - 1: image_index += 1
+    current_layers = [np.copy(l) for l in images[image_index]]
+    current_display = current_layers[layer_index]
 
   # pointer
   elif k == 45 or k == ord(']'):
@@ -122,7 +142,8 @@ while True:
 
   # reset
   elif k == ord('r'):
-    current_display = current_layer[display_index]
+    current_layers = [np.copy(l) for l in images[image_index]]
+    current_display = current_layers[layer_index]
 
   # exit
   elif k == 27 or k == ord('q'):
@@ -136,7 +157,7 @@ while True:
 
   # update cursor
   # overlay
-  overlay = current_layer[display_index].copy()
+  overlay = current_layers[layer_index].copy()
 
   # -1 to fill the circle
   cv2.circle(overlay, (mouse_x, mouse_y), MOUSE_RADIUS, FILL_COLOUR, -1) 
